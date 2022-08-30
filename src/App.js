c3b1b8d5aaf08eefa3714ef0bad7e9c0aea3aee1
Partha Bora <Partha.Bora@marlabs.com>
2022-08-30 23:17:12 +0530

    3 commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Greet from './components/Greet';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <Greet/>
         <Greet/>
         <Greet/>
+        <Welcome/>
       </header>
     </div>
   );
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Welcome extends Component {
+  render() {
+    return (
+      <div>Hello James from Class Component</div>
+    )
+  }
+}
+
+export default Welcome
