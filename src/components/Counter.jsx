8d455ed626bb0c2c4391297826ab8d2f332c5eb9
Partha Bora <Partha.Bora@marlabs.com>
2022-08-31 23:45:11 +0530

    7 commit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -7,19 +7,39 @@ export class Counter extends Component {
       this.state = {
          count: 0
       }
+
+      this.incrementFive = this.incrementFive.bind(this)
     }
   increment(){
-    this.setState({
-        count: this.state.count + 1
-    }, ()=> {console.log('Callback:',this.state.count)})
+    // this.setState({
+    //     count: this.state.count + 1
+    // }, ()=> {console.log('Callback:',this.state.count)})
+    
+    // console.log(this.state.count)
+
+
+
+    this.setState(prevState => ({
+        count: prevState.count + 1
+    }), ()=> {console.log('Callback:',this.state.count)})
     
     console.log(this.state.count)
+  }
+  incrementFive(){
+    this.increment()
+    this.increment()
+    this.increment()
+    this.increment()
+    this.increment()
   }
   render() {
     return (
       <>
         <div>Counter:{this.state.count}</div>
         <button onClick={()=> this.increment()}>Increment</button>
+        <button onClick={()=> this.incrementFive()}>Increment5</button>
+        <button onClick={this.incrementFive.bind(this)}>Increment5</button>
+        <button onClick={this.incrementFive}>Increment5</button>
       </>
     )
   }
